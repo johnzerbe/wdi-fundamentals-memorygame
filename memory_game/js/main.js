39cc0539638766ea6319dadb98798776eb76cefe
John Zerbe <jczerbe@MacBook-Pro-3.local>
2019-06-17 13:26:41 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,7 +20,9 @@ var cards = [
 	cardImage: "images/king-of-diamond.png",
 }
 ];
+
 var cardsInPlay = [];
+
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
@@ -30,17 +32,30 @@ function checkForMatch(){
 		alert("Sorry, try again.");
 
 	}
-}
+};
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
+ 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	if (cardsInPlay.length === 2)
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2); {
 	checkForMatch();
-}
+	}
+};
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
 
 
-flipCard(0);
-flipCard(2);
